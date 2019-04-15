e13f9bcf1a062a1ab99e7afecbb32f45fc87eefb
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 23:20:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,6 @@ def apply_coupons(cart, coupons)
       else
         hash["#{item} W/COUPON"][:count] += 1
       end
-
       hash[item][:count] -= coupon_hash[:num]
     end
   end
@@ -48,13 +47,9 @@ def checkout(items, coupons)
   cart = consolidate_cart(items)
   cart1 = apply_coupons(cart, coupons)
   cart2 = apply_clearance(cart1)
-
   total = 0
-
   cart2.each do |name, price_hash|
     total += price_hash[:price] * price_hash[:count]
   end
-
   total > 100 ? total * 0.9 : total
-
 end
