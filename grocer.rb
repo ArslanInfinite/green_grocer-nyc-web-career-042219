a979ed8e136d32ec364afa6c953154f8441e900b
Arslan Ali <ali.arslan319@gmail.com>
2019-04-15 23:11:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,9 @@
 def consolidate_cart(cart)
-  # code here
+  [].tap do |cart|
+		rand(20).times do
+			cart.push(items.sample)
+		end
+	end
 end
 
 def apply_coupons(cart, coupons)
@@ -12,4 +16,12 @@ end
 
 def checkout(cart, coupons)
   # code here
+end
+
+def generate_cart
+	[].tap do |cart|
+		rand(20).times do
+			cart.push(items.sample)
+		end
+	end
 end
